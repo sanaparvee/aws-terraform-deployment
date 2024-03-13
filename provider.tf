b43830e03720778e99b305be5f043e93b4ff98b0
sanaparvee <127944750+sanaparvee@users.noreply.github.com>
2024-03-13 15:19:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~>1.6"
+    #required_version = "~>1.6"
     backend "s3" {
     bucket = "bro-test-bucket"
     key    = "statefile"
@@ -11,15 +11,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      required_version = "~>5.23"
+     # required_version = "~>5.23"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  assume_role {
-    role_arn = var.assume_role_arn
-    external_id = "terraform-workflow"
-  }
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
 }
