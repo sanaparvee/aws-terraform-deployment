b5e55bb6ce6edfd9662a84d691530a5d48a6e26b
sanaparvee <127944750+sanaparvee@users.noreply.github.com>
2024-03-13 15:35:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
+locals {
+  region_prefix = var.prefix
+}
+
 resource "aws_s3_bucket" "s3" {
-  bucket = "sanyel-test-bucket-${var.prefix}"
+  bucket = "sanyel-test-bucket-${local.prefix}"
   tags = {
     Name        = "this bucket"
     Environment = "Demo"
