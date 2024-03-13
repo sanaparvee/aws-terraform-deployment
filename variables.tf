86b6c6734b06b4ff7dcf76c697fb25f7a1f4eb40
sanaparvee <127944750+sanaparvee@users.noreply.github.com>
2024-03-13 14:51:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "assume_role_arn" {
-  default = "arn:aws:iam::${aws_account_id}:role/aws-terraform-deployment"
+  default = "arn:aws:iam::${var.aws_account_id}:role/aws-terraform-deployment"
+}
+
+variable "aws_account_id" {
+  default = "283854207187"
 }
